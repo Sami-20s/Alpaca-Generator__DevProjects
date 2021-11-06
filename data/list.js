d0161bd1ba3021566b92cc90e40b8ea6f60440a5
Sami-20s <samikfouri22@gmail.com>
2021-11-06 21:56:24 +0200

    🚀 Init The Project

---
+++ data/list.js
@@ -0,0 +1,328 @@
+// Hair
+export const Hair = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/hair/default.png",
+    },
+    {
+        id: 2,
+        text: "Bang",
+        url: "/alpaca/hair/bang.png",
+    },
+    {
+        id: 3,
+        text: "Curls",
+        url: "/alpaca/hair/curls.png",
+    },
+    {
+        id: 4,
+        text: "Elegant",
+        url: "/alpaca/hair/elegant.png",
+    },
+    {
+        id: 5,
+        text: "Fancy",
+        url: "/alpaca/hair/fancy.png",
+    },
+    {
+        id: 6,
+        text: "Short",
+        url: "/alpaca/hair/short.png",
+    },
+];
+export const Ears = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/ears/default.png",
+    },
+    {
+        id: 2,
+        text: "Tilt-backward",
+        url: "/alpaca/ears/tilt-backward.png",
+    },
+    {
+        id: 3,
+        text: "Tilt-forward",
+        url: "/alpaca/ears/tilt-forward.png",
+    },
+];
+export const Eyes = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/eyes/default.png",
+    },
+    {
+        id: 2,
+        text: "Angry",
+        url: "/alpaca/eyes/angry.png",
+    },
+    {
+        id: 3,
+        text: "Naughty",
+        url: "/alpaca/eyes/naughty.png",
+    },
+    {
+        id: 4,
+        text: "Panda",
+        url: "/alpaca/eyes/panda.png",
+    },
+    {
+        id: 5,
+        text: "Smart",
+        url: "/alpaca/eyes/smart.png",
+    },
+    {
+        id: 6,
+        text: "Star",
+        url: "/alpaca/eyes/star.png",
+    },
+];
+export const Mouth = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/mouth/default.png",
+    },
+    {
+        id: 2,
+        text: "Astonished",
+        url: "/alpaca/mouth/astonished.png",
+    },
+    {
+        id: 3,
+        text: "Eating",
+        url: "/alpaca/mouth/eating.png",
+    },
+    {
+        id: 4,
+        text: "Laugh",
+        url: "/alpaca/mouth/laugh.png",
+    },
+    {
+        id: 5,
+        text: "Tongue",
+        url: "/alpaca/mouth/tongue.png",
+    },
+];
+// Neck
+export const Neck = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/neck/default.png",
+    },
+    {
+        id: 2,
+        text: "Bend-backward",
+        url: "/alpaca/neck/bend-backward.png",
+    },
+    {
+        id: 3,
+        text: "Bend-forward",
+        url: "/alpaca/neck/Bend-forward.png",
+    },
+    {
+        id: 4,
+        text: "Thick",
+        url: "/alpaca/neck/thick.png",
+    },
+];
+// Leg
+export const Leg = [
+    {
+        id: 1,
+        text: "Default",
+        url: "/alpaca/leg/default.png",
+    },
+    {
+        id: 2,
+        text: "Cookie",
+        url: "/alpaca/leg/cookie.png",
+    },
+    {
+        id: 3,
+        text: "Bubble-tea",
+        url: "/alpaca/leg/bubble-tea.png",
+    },
+    {
+        id: 4,
+        text: "Game-console",
+        url: "/alpaca/leg/game-console.png",
+    },
+    {
+        id: 5,
+        text: "Tilt-backward",
+        url: "/alpaca/leg/tilt-backward.png",
+    },
+    {
+        id: 6,
+        text: "Tilt-forward",
+        url: "/alpaca/leg/tilt-forward.png",
+    },
+];
+// Accessories
+export const Accessories = [
+    {
+        id: 5,
+        text: "Default",
+        url: "",
+    },
+    {
+        id: 1,
+        text: "Earings",
+        url: "/alpaca/accessories/earings.png",
+    },
+    {
+        id: 2,
+        text: "Flower",
+        url: "/alpaca/accessories/flower.png",
+    },
+    {
+        id: 3,
+        text: "Glasses",
+        url: "/alpaca/accessories/glasses.png",
+    },
+    {
+        id: 4,
+        text: "Headphone",
+        url: "/alpaca/accessories/headphone.png",
+    },
+];
+// Backgrounds
+export const Backgrounds = [
+    {
+        id: 1,
+        text: "Blue-1",
+        url: "/alpaca/backgrounds/blue50.png",
+    },
+    {
+        id: 2,
+        text: "Blue-2",
+        url: "/alpaca/backgrounds/blue60.png",
+    },
+    {
+        id: 3,
+        text: "Blue-3",
+        url: "/alpaca/backgrounds/blue70.png",
+    },
+    {
+        id: 4,
+        text: "Darkblue-1",
+        url: "/alpaca/backgrounds/darkblue30.png",
+    },
+    {
+        id: 5,
+        text: "Darkblue-2",
+        url: "/alpaca/backgrounds/darkblue50.png",
+    },
+    {
+        id: 6,
+        text: "Darkblue-3",
+        url: "/alpaca/backgrounds/darkblue70.png",
+    },
+    {
+        id: 7,
+        text: "Green-1",
+        url: "/alpaca/backgrounds/green50.png",
+    },
+    {
+        id: 8,
+        text: "Green-2",
+        url: "/alpaca/backgrounds/green60.png",
+    },
+    {
+        id: 9,
+        text: "Green-3",
+        url: "/alpaca/backgrounds/green70.png",
+    },
+    {
+        id: 10,
+        text: "Grey-1",
+        url: "/alpaca/backgrounds/grey40.png",
+    },
+    {
+        id: 11,
+        text: "Grey-2",
+        url: "/alpaca/backgrounds/grey70.png",
+    },
+    {
+        id: 12,
+        text: "Grey-3",
+        url: "/alpaca/backgrounds/grey80.png",
+    },
+    {
+        id: 13,
+        text: "Red-1",
+        url: "/alpaca/backgrounds/red50.png",
+    },
+    {
+        id: 14,
+        text: "Red-2",
+        url: "/alpaca/backgrounds/red60.png",
+    },
+    {
+        id: 15,
+        text: "Red-3",
+        url: "/alpaca/backgrounds/red70.png",
+    },
+    {
+        id: 16,
+        text: "Yellow-1",
+        url: "/alpaca/backgrounds/yellow50.png",
+    },
+    {
+        id: 17,
+        text: "Yellow-2",
+        url: "/alpaca/backgrounds/yellow60.png",
+    },
+    {
+        id: 18,
+        text: "Yellow-3",
+        url: "/alpaca/backgrounds/yellow70.png",
+    },
+];
+export const accList = [
+    {
+        id: 1,
+        text: "Hair",
+        url: Hair,
+    },
+    {
+        id: 2,
+        text: "Ears",
+        url: Ears,
+    },
+    {
+        id: 3,
+        text: "Eyes",
+        url: Eyes,
+    },
+    {
+        id: 4,
+        text: "Mouth",
+        url: Mouth,
+    },
+    {
+        id: 5,
+        text: "Neck",
+        url: Neck,
+    },
+    {
+        id: 6,
+        text: "Leg",
+        url: Leg,
+    },
+    {
+        id: 7,
+        text: "Accessories",
+        url: Accessories,
+    },
+    {
+        id: 8,
+        text: "Backgrounds",
+        url: Backgrounds,
+    },
+];
---
+++ pages/index.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import {
+    accList,
+    Hair,
+    Ears,
+    Eyes,
+    Mouth,
+    Neck,
+    Leg,
+    Accessories,
+    Backgrounds,
+} from "../data/list";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Home = () => {
+    const [style, setStyle] = useState(Hair);
+    const [hair, setHair] = useState("/alpaca/hair/default.png");
+    const [ears, setEars] = useState("/alpaca/ears/default.png");
+    const [eyes, setEyes] = useState("/alpaca/eyes/default.png");
+    const [mouth, setMouth] = useState("/alpaca/mouth/default.png");
+    const [neck, setNeck] = useState("/alpaca/neck/default.png");
+    const [leg, setLeg] = useState("/alpaca/leg/default.png");
+    const [accessories, setAccessories] = useState("");
+    const [backgrounds, setBackgrounds] = useState(
+        "/alpaca/backgrounds/blue50.png"
+    );
+
+    const handleStyle = url => {
+        switch (style) {
+            case Hair:
+                setHair(url);
+                break;
+            case Ears:
+                setEars(url);
+                break;
+            case Eyes:
+                setEyes(url);
+                break;
+            case Mouth:
+                setMouth(url);
+                break;
+            case Neck:
+                setNeck(url);
+                break;
+            case Leg:
+                setLeg(url);
+                break;
+            case Accessories:
+                setAccessories(url);
+                break;
+            case Backgrounds:
+                setBackgrounds(url);
+                break;
+            default:
+                break;
+        }
+    };
+
+    return (
+        <Container>
+            <h1>ALPACA GENERATOR</h1>
+            <Row xs="1" md="2" className="d-flex justify-content-center">
+                <Col>
+                    <div
+                        className="img-container"
+                        style={{ backgroundImage: `url(${backgrounds})` }}
+                    >
+                        <img src={hair} alt="" className="hair__img" />
+                        <img src={ears} alt="" className="ears__img" />
+                        <img src={eyes} alt="" className="eyes__img" />
+                        <img src={mouth} alt="" className="mouth__img" />
+                        <img src={neck} alt="" className="neck__img" />
+                        <img src={leg} alt="" className="leg__img" />
+                        <img
+                            src={accessories}
+                            alt=""
+                            className="accessories__img"
+                        />
+                        <img
+                            src="/alpaca/nose.png"
+                            alt=""
+                            className="nose__img"
+                        />
+                    </div>
+                    <Row>
+                        <Col>
+                            <Button>Random</Button>
+                        </Col>
+                        <Col>
+                            <Button>Download</Button>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col>
+                    <div>
+                        <h4>Accessories The ALPACA</h4>
+                        <Row xs="2" md="2" lg="3" className="g-4 mt-3">
+                            {accList.map(item => {
+                                return (
+                                    <div key={item.id}>
+                                        <Button
+                                            variant="outline-primary"
+                                            size="lg"
+                                            onClick={() => setStyle(item.url)}
+                                        >
+                                            {item.text}
+                                        </Button>
+                                    </div>
+                                );
+                            })}
+                        </Row>
+                    </div>
+                    <div className="mt-4">
+                        <h4>Style</h4>
+                        <Row xs="2" md="2" lg="3" className="g-4 mt-3">
+                            {style.map(item => {
+                                return (
+                                    <div key={item.id}>
+                                        <Button
+                                            variant="outline-secondary"
+                                            size="lg"
+                                            onClick={() =>
+                                                handleStyle(item.url)
+                                            }
+                                        >
+                                            {item.text}
+                                        </Button>
+                                    </div>
+                                );
+                            })}
+                        </Row>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Home;
